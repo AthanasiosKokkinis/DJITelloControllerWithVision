4e906c569d66a0bb9e46b2a0a4e4be17d851680a
AthanasiosKokkinis <70268936+AthanasiosKokkinis@users.noreply.github.com>
2022-03-20 20:24:32 +0200

    Update telloControllerThanos.py

--- telloControllerThanos.py
+++ telloControllerThanos.py
@@ -4,67 +4,67 @@ from pynput import keyboard
 from threading import Thread
 #left_right=0
 
-def initializeTello(): # Gia na kanw arxikopoihsh drone
-    tello_drone=Tello() # Antikeimeno
-    tello_drone.connect() # Sundesh
-    print(tello_drone.get_battery()) # Mas dinei thn mpataria
-    tello_drone.for_back_velocity=0 # Mhdenizw arxikes taxuthtes
+def initializeTello(): # Drone initialization
+    tello_drone=Tello()
+    tello_drone.connect() # Connect to drone
+    print(tello_drone.get_battery()) # Battery percentage
+    tello_drone.for_back_velocity=0 # Make initial velocities zero
     tello_drone.left_right_velocity=0
     tello_drone.up_down=0
     tello_drone.yaw_velocity=0
     tello_drone.speed=0
-    tello_drone.steamoff() # Stamataw to stream an einai anoikto 
+    tello_drone.steamoff() # Stop video streaming if open
     return trello_drone
 
-def shareVision(): # Dwse video
+def shareVision(): # Function for video streaming
     global drone
-    drone.streamon() # Anoigw to stream
-    frame_read=drone.get_frame_read() # Pairnw ta frames gia panta
+    drone.streamon() # Open the stream
+    frame_read=drone.get_frame_read() # This is a callback function which returns the frames 
     recording=True
-    while recording: # Den eimai sigouros oti xreiazetai
-        cv2.imshow("Tello vision",frame_read.frame) # Mallon den xreiazetai na kanw ontws lhpsh neou frame
-        sleep(1/30) # 8ewrhtika 30 fps
+    while recording:
+        cv2.imshow("Tello vision",frame_read.frame) # Show the frame
+        sleep(1/30) # Limit video to 30fps
 
 def drone_handler(key): # Controller
-    global drone # Pragmatika, h Python einai toso xalia pou me anagkazei na xrhsimopoiw global, gt to keyboard den me afhnei na kanw polla....
-    step=1
-    speed=10
-    angle_step=5
+    global drone # Global drone object
+    step=1 # Step for translational movement, might get deleted in future updates.
+    speed=10 # Speed of movement
+    angle_step=5 # Step for rotational movement
     if key==keyboard.Key.esc:
-        drone.land() # Prosgeiwsh
+        drone.land() # Land the drone
         exit()
     elif key==keyboard.Key.d:
-        drone.go_xyz_speed(step,0,0,speed) # Fantasou na exw kanei patata me tis dieu8unseis xD
+        drone.go_xyz_speed(step,0,0,speed) # Move to the right
         print("Right")
     elif key==keyboard.Key.w:
-        drone.go_xyz_speed(0,step,0,speed)
+        drone.go_xyz_speed(0,step,0,speed) # Move forward
         print("Forward")
     elif key.char==keyboard.Key.a:
-        drone.go_xyz_speed(-step,0,0,speed)
+        drone.go_xyz_speed(-step,0,0,speed) # Move to the keft
         print("Left")
     elif key==keyboard.Key.s:
-        drone.go_xyz_speed(0,-step,0,speed)
+        drone.go_xyz_speed(0,-step,0,speed) # Move backwards
         print("Back")
     elif key==keyboard.Key.space:
-        drone.go_xyz_speed(0,0,step,speed)
+        drone.go_xyz_speed(0,0,step,speed) # Go up
         print("Up")
     elif key==keyboard.Key.shift_l:
-        drone.go_xyz_speed(0,0,-step,speed)
+        drone.go_xyz_speed(0,0,-step,speed) # Go down
         print("Down")
     elif key=='e':
-        drone.rotate_clockwise(angle_step)
+        drone.rotate_clockwise(angle_step) # Rotate clockwise
         print("Clockwise rotation")
     elif key=='q':
-        drone.rotate_counter_clockwise(angle_step)
+        drone.rotate_counter_clockwise(angle_step) # Rotate anti-clockwise
         print("Counter clockwise rotation")
 
-def main(): # Main sunarthsh
+def main(): # Main fucntion
     drone=initializeTello() # Drone init
-    drone.takeoff() # Flyyy
-    keyboard_listener=keyboard.Listener(on_press=drone_handler) # Poia sunarthsh 8a akouei
-    keyboard_listener.start() # Thread gia keyboard
-    vision_thread=Thread(target=shareVision)
-    vision_thread.start() # Thread gia camera
+    drone.takeoff() # Fly
+    keyboard_listener=keyboard.Listener(on_press=drone_handler) # Thread for keyboard
+    keyboard_listener.start() 
+    vision_thread=Thread(target=shareVision) # Vision thread
+    vision_thread.start()
     
 main()
 
